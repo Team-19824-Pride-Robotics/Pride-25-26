bf906da6ddc1b0fff53c97b7113273451fadf175
CoolRobots <darppdudee@gmail.com>
2025-11-09 19:25:48 -0600

    Small teleop update
    added field centric drive and emergency artifact ejection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prideRobotics/redTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prideRobotics/redTeleop.java
@@ -2,12 +2,15 @@ package org.firstinspires.ftc.teamcode.prideRobotics;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.arcrobotics.ftclib.util.InterpLUT;
 import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.prideRobotics.subsystems.intake;
 import org.firstinspires.ftc.teamcode.prideRobotics.subsystems.flywheel;
 import org.firstinspires.ftc.teamcode.prideRobotics.subsystems.ballKickers;
@@ -23,7 +26,8 @@ public class redTeleop extends LinearOpMode {
    private ballKickers ballKickers;
     private limelight limelight;
     private transferChanneler transferChanneler;
-//logic variable declarations
+//fun variables
+    private static double strafeFix=1;
     private boolean launchLeft=false;
     private boolean launchRight=false;
     private double launchVel;
@@ -52,11 +56,15 @@ public class redTeleop extends LinearOpMode {
 
         // Declare motor ok
         // Absolutely yes make ID's match configuration
+        IMU imu = hardwareMap.get(IMU.class, "imu");
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("fLD");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("bLD");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("fRD");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("bRD");
-
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
         //reverse drive motors
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -88,14 +96,24 @@ public class redTeleop extends LinearOpMode {
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS); //todo: check control hub model
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * strafeFix;
+
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
 
             frontLeftMotor.setPower(frontLeftPower * (1-gamepad1.right_trigger));
             backLeftMotor.setPower(backLeftPower * (1-gamepad1.right_trigger));
@@ -130,8 +148,13 @@ public class redTeleop extends LinearOpMode {
                 launchLeft=false;
                 launchRight=false;
             }
+
+
             if(launchRight||launchLeft){
-                if(limelight.getDistance()==-1){
+                if(gamepad1.start){
+                    launchVel=600;
+                }
+                else if(limelight.getDistance()==-1){
                     launchVel=1000;
                 }else{
                     launchVel=lut.get(limelight.getDistance());
@@ -139,6 +162,7 @@ public class redTeleop extends LinearOpMode {
             }  else{
                 launchVel=600;
             }
+
 
             if(launchRight) {
                 ballKickers.retractLeft();
