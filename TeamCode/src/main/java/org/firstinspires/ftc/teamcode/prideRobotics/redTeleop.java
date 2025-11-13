ae7519c52f7f62422cb034650b7bde800f742c36
IvanFortenberry <ivan_fortenberry.stu@willowschoolnola.org>
2025-11-13 15:13:26 -0600

    Update redTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prideRobotics/redTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prideRobotics/redTeleop.java
@@ -96,8 +96,8 @@ public class redTeleop extends LinearOpMode {
 
         while (opModeIsActive()) {
             //drive control
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x;
+            double x = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double y = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
             if (gamepad1.options) {
